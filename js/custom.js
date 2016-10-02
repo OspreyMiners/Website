4d5ba4e2742469448074713308fd2c310f0413e8
NagaKanaparthy <nkanaparthyper@live.com>
2016-10-02 08:50:44 -0400

    2015 to 2016

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// NIVO LIGHTBOX
+$('light').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+   $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
